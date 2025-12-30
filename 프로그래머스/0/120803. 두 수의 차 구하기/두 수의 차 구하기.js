4df7a6301fd0d154253cf49aa44e46a0d71169c5
해원 <khcw1029@daum.net>
2025-12-30 17:13:07 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.js
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.js
@@ -1,4 +1,10 @@
-function solution(num1, num2) {
-    var answer = num1 - num2
-    return answer;
-}
+// 내가 작성한 코드
+// function solution(num1, num2) {
+//     var answer = num1 - num2
+//     return answer;
+// }
+
+// 화살표 함수로 작성할 수도 있음
+// 코드량이 줄어서 좋은 듯
+const solution = (num1, num2) => num1 - num2
+
