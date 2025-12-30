5efe949935e01984fb81508e1a9542da5d908580
해원 <khcw1029@daum.net>
2025-12-30 18:00:26 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,6 @@
+// function solution(angle) {
+//     var answer = 0;
+//     return answer;
+// }
+
+const solution = (angle) => angle === 180 ? 4 : angle > 90 && angle < 180 ? 3 : angle === 90 ? 2 : 1 
