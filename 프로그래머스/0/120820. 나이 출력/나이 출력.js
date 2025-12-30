6a568835494d97e2871737fab8a09b1b2c49832d
해원 <khcw1029@daum.net>
2025-12-30 17:45:44 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.js
@@ -0,0 +1,9 @@
+// function solution(age) {
+//     var answer = 0;
+//     return answer;
+// }
+
+const solution = (age) => 2023 - age;
+
+// 만약 특정 연도가 아닌 현재 연도라는 기준이 있었다면
+// new Date().getFullYear() 이걸 써서 해야 함
