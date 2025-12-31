2703ac9b13e080eea22bc39d16919401ca4e5394
해원 <khcw1029@daum.net>
2025-12-31 10:13:37 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,10 +1,13 @@
-function solution(n, k) {
-    let sum = 0;
-    let s = Math.trunc(n / 10);
-    let v = 0;
+// function solution(n, k) {
+//     let sum = 0;
+//     // let s = Math.trunc(n / 10);
+//     let v = 0;
 
-    s < k ? v = k - s : v = 0
+//     Math.trunc(n / 10) < k ? v = k - Math.trunc(n / 10) : v = 0
     
-    sum = n * 12000 + v * 2000
-    return sum;
-}
+//     Math.trunc(n / 10) < k ?
+//     sum = n * 12000 + v * 2000
+//     return sum;
+// }
+
+const solution = (n, k) => Math.trunc(n / 10) < k ? n * 12000 + (k - Math.trunc(n / 10)) * 2000 : n * 12000
