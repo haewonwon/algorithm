2f1c17fbfda8ffd8bfeedeaefeee1e34ca1b3064
해원 <khcw1029@daum.net>
2025-12-31 15:18:27 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,3 +1,7 @@
 function solution(numbers) {
     return numbers.reduce((sum, dig) => sum + dig, 0) / numbers.length
 }
+
+// numbers.reduce((sum, dig) => sum + dig, 0) / numbers.length
+// 배열을 돌면서 하나로 응축해라!
+// sum은 총합, dig는 현재 숫자, 초기값은 0, 배열의 길이로 나눔
