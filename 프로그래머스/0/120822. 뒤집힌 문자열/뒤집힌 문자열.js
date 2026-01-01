752f5d3417fa6e4913d83c715ed335512b2a2840
해원 <khcw1029@daum.net>
2026-01-01 20:27:16 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
@@ -1,3 +1,7 @@
 function solution(my_string) {
-    return my_string.split("").reverse().join("");
+    // 내가 작성한 답
+    // return my_string.split("").reverse().join("");
+    
+    // 참고한 답
+    return [...my_string].reverse().join("");
 }
