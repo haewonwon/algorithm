a939a97d6695a1acceedf67d51d6b8440772f323
해원 <khcw1029@daum.net>
2026-01-03 23:02:35 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,3 +1,6 @@
 function solution(slice, n) {
-    return n <= slice ? 1 : n % slice === 0 ? n/slice : Math.floor(n/slice) + 1
+    // return n <= slice ? 1 : n % slice === 0 ? n/slice : Math.floor(n/slice) + 1
+    
+    // 올림하는 방법으로 풀기, Math.ceil() 공부하기
+    return Math.ceil(n / slice);
 }
