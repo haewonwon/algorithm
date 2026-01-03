0290bce1f739bfa92e0d4ecc962d80c0193c90ea
해원 <khcw1029@daum.net>
2026-01-03 18:19:03 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,4 +1,9 @@
 function solution(sides) {
     let array = sides.sort((a, b) => b - a)
     return array[0] < array[1] + array[2] ? 1 : 2;
+    
+    // 다른 사람이 작성한 코드 중 명시적인 코드, 이런 사고도 배우면 좋을 듯
+    // 구조 분해 할당을 이용한 코드
+    // const [long, a, b] = sides.sort((a,b) => b-a);
+    // return long < a + b ? 1 : 2
 }             
