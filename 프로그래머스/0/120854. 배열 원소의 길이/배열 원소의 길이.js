3490057110cea4d79c92ca7c9cc5cfed21b07758
해원 <khcw1029@daum.net>
2026-01-06 23:03:26 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,9 +1,15 @@
 function solution(strlist) {
-    let arr = []
+    // 내가 작성한 코드
+    // push()인데 push[]로 작성해서 arr에 요소가 추가되지 않음 괄호 구분 잘하기
     
-    for (let i = 0; i < strlist.length; i++) {
-        arr.push(strlist[i].length)
-    }
+    // let arr = []
     
-    return arr
+    // for (let i = 0; i < strlist.length; i++) {
+    //     arr.push(strlist[i].length)
+    // }
+    
+    // return arr
+    
+    // 참고 코드
+    return strlist.map((el) => el.length)
 }
