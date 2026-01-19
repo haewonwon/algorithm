4d3b70a799133e4209031de59186fbbbf003d963
해원 <khcw1029@daum.net>
2026-01-20 03:01:27 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    let arr = [...my_string]
+    let result = ""
+    
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i] === arr[i].toUpperCase()) {
+            result += arr[i].toLowerCase()
+        } else {
+            result += arr[i].toUpperCase()
+        }
+    }
+    
+    return result
+}
