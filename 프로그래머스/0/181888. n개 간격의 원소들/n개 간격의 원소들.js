00938c66327544513888c926894355aa4648597f
해원 <khcw1029@daum.net>
2026-01-24 02:53:24 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,11 +1,14 @@
 function solution(num_list, n) {
-    let arr = []
+//     let arr = []
     
-    for (let i = 0; i < num_list.length; i++) {
-        if (i % n === 0) {
-            arr.push(num_list[i])
-        }
-    }
+//     for (let i = 0; i < num_list.length; i++) {
+//         if (i % n === 0) {
+//             arr.push(num_list[i])
+//         }
+//     }
     
-    return arr
+//     return arr
+    
+    // filter를 이용해 index 조건에 맞는 배열 반환하는 방법
+    return num_list.filter((_, i) => i % n === 0)
 }
