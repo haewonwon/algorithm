9c50bb6c8d93719f3e25459cb5e828d2cbf5bd1b
해원 <khcw1029@daum.net>
2026-01-31 19:31:46 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,16 +1,25 @@
 function solution(rsp) {
-    let arr = [...rsp]
-    let newArr = []
+//     let arr = [...rsp]
+//     let newArr = []
     
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === "0") {
-            newArr.push("5")
-        } else if (arr[i] === "2") {
-            newArr.push("0")
-        } else {
-            newArr.push("2")
-        }
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === "0") {
+//             newArr.push("5")
+//         } else if (arr[i] === "2") {
+//             newArr.push("0")
+//         } else {
+//             newArr.push("2")
+//         }
+//     }
+    
+//     return newArr.join("")
+    
+    // 각 대응하는 게 있으면 객체로 풀기
+    let arr = {
+        0: 5,
+        2: 0,
+        5: 2,
     }
     
-    return newArr.join("")
+    return [...rsp].map((n) => arr[n]).join("")
 }
