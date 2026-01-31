7252881f998cbf448904f435897b0778ea3e6b35
해원 <khcw1029@daum.net>
2026-01-31 19:29:19 +0900

    [level 0] Title: 가위 바위 보, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,16 @@
+function solution(rsp) {
+    let arr = [...rsp]
+    let newArr = []
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "0") {
+            newArr.push("5")
+        } else if (arr[i] === "2") {
+            newArr.push("0")
+        } else {
+            newArr.push("2")
+        }
+    }
+    
+    return newArr.join("")
+}
